084d46d96521490d2597d37e04920886c438c2ec
kidlatpogi <dzeref4000@gmail.com>
2025-10-29 13:43:41 +0800

    fix: correct entry and import paths for Vite/React app

---
+++ src/JSX/App.jsx
@@ -0,0 +1,12 @@
+import '../CSS/App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Welcome to My Portfolio</h1>
+      <p>This is a brief introduction about myself.</p>
+    </>
+  )
+}
+
+export default App
