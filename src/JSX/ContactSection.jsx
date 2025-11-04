52dece266b66cb08320da52296cf348f287869a0
kidlatpogi <dzeref4000@gmail.com>
2025-11-04 21:58:23 +0800

    fix(contact): correct resume link to deployed filename

--- src/JSX/ContactSection.jsx
+++ src/JSX/ContactSection.jsx
@@ -91,7 +91,7 @@ function ContactSection({ contactStatus, onSubmit }) {
                 <span className='Contact-link-desc'>Professional profile</span>
               </a>
               
-              <a className='Contact-link' href='/RESUME SAMPLE.pdf' download='Zeus_Angelo_Bautista_Resume.pdf'>
+              <a className='Contact-link' href='/Zeus_Angelo_Bautista_Resume.pdf' download='Zeus_Angelo_Bautista_Resume.pdf'>
                 <span className='Contact-link-icon'>
                   <svg width='18' height='18' viewBox='0 0 24 24' fill='none' stroke='currentColor' strokeWidth='2' strokeLinecap='round' strokeLinejoin='round'>
                     <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
