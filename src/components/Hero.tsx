bf08061a4117cedc8fde78d41e77ab09e83356c6
kidlatpogi <dzeref4000@gmail.com>
2026-06-21 12:25:57 +0800

    Changed the color of Connect, Submit Inquirt. Change opacity of Future to 1, Added Work experience to Assisstant

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -47,7 +47,7 @@ export default function Hero() {
                 </div>
 
                 {/* Circular Profile Holder (empty grey circle with accent border) */}
-                <div 
+                <div
                   className="w-14 h-14 sm:w-20 sm:h-20 md:w-32 md:h-32 lg:w-40 lg:h-40 rounded-full border-[2px] sm:border-[3px] md:border-[5px] border-accent bg-[#E5E5E5] flex-shrink-0 ring-4 sm:ring-6 md:ring-[10px] ring-accent/10"
                 />
               </div>
@@ -57,7 +57,7 @@ export default function Hero() {
             </h1>
             <h1 className="font-clash-semibold text-5xl sm:text-6xl md:text-7xl lg:text-[7.5rem] xl:text-[8.5rem] font-semibold text-accent tracking-tighter leading-[0.9] select-none whitespace-nowrap">
               <span className="relative inline-block italic">
-                <span className="opacity-50">Future</span>
+                <span>Future</span>
                 <span className="absolute left-0 right-0 top-[55%] -translate-y-1/2 h-[3px] sm:h-[4px] md:h-[6px] lg:h-[8px] bg-black rounded-full pointer-events-none" />
               </span>{" "}
               Developer
